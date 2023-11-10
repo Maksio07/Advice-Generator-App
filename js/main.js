65f3152cc175e7116dcfa0e6b91eaa598e945734
Maks <mmaksimov20000806@gmail.com>
2023-11-10 17:06:52 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const adviceArea = document.querySelector('.advice-app__advice')
+const adviceBtn = document.querySelector('.advice-app__button')
+const adviceNumber = document.querySelector('.advice-app__title--number');
+
+const API_LINK = 'https://api.adviceslip.com/advice'
+
+const generateAdvice = () => {
+	fetch(API_LINK)
+		.then(res => res.json())
+		.then(data => {
+			const adviceID = data.slip.id;
+			const advice = data.slip.advice;
+			adviceNumber.textContent = adviceID;
+			adviceArea.textContent = advice;
+		})
+		.catch(() => {
+			adviceArea.style.color = '#cf1717'
+			adviceArea.textContent = 'Something was wrong 😕'
+		})
+}
+
+adviceBtn.addEventListener('click', generateAdvice)
